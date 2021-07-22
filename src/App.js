b31ad262ff8fead4337cd8428a319fa9f09d4841
Jayanthi <jayanthi.ragu93@gmail.com>
2021-07-22 15:35:01 +0530

    updated_bug

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       to:new Date(),
       show_price:false,
       totalPrice:0,
-      selectedValue:null,
+      selectedValue:data[0].name,
       mileage:null,
       searchValue:''
     }
@@ -55,16 +55,28 @@ class App extends Component {
 
   ProceedBooking=()=>{
     var num_of_days=Math.ceil((this.state.to-this.state.from)/8.64e7);
-    var price,minimum_rent_period;
+    var price,minimum_rent_period,type,durability;
     data.map((data,index)=>{
       if(data.name==this.state.selectedValue){
         price=data.price;
         minimum_rent_period=data.minimum_rent_period;
+        type=data.type;
+        durability=data.durability;
       }
     })
 
-    //Price Calculation for rental
-    this.setState({totalPrice:(num_of_days*price*minimum_rent_period),show_price:true,book:false});
+    //Durability
+    if(type.includes("p"))
+    {
+      durability=durability-1;
+    }
+    else if(type.includes("m")){
+      durability=durability-2;
+    }
+    durability=(durability-2)*num_of_days;
+
+    //Price Calculation for rental including meter estimation
+    this.setState({totalPrice:(num_of_days*10*price*minimum_rent_period),show_price:true,book:false});
   }
 
   ConfirmBooking=()=>{
@@ -97,6 +109,39 @@ class App extends Component {
         <div class="search">
           Search:<input type="text" value={this.state.searchValue} onChange={this.search}></input>
         </div>
+        {this.state.book &&<div class="book">Book a Product
+              <select id="dropdown" onChange={this.onDropdown}>
+              {data.map((data,index)=>(
+                <React.Fragment key={index}>
+                <option value={data.name}>{data.name}</option>
+                </React.Fragment>
+              ))}
+              </select>
+              From:<DatePicker datefromat="MM/dd/yyyy" selected={this.state.from} onChange={this.onFromDate} /><br></br>
+              To:<DatePicker datefromat="MM/dd/yyyy" selected={this.state.to} onChange={this.onToDate} /><br></br>
+              <button type="submit" onClick={this.ProceedBooking}>Proceed</button>
+              <button type="submit" onClick={this.CancelBooking}>Cancel</button>
+            </div>}
+
+            {this.state.return &&<div class="book">Return a Product
+              <select id="dropdown" onChange={this.onDropdown}>
+              {data.map((data,index)=>(
+                <React.Fragment key={index}>
+                <option value={data.name}>{data.name}</option>
+                </React.Fragment>
+              ))}
+              </select>
+              Mileage:<input type="text" onChange={this.onMileageChange}></input>
+              <button type="submit" onClick={this.ProceedReturn}>Proceed</button>
+              <button type="submit" onClick={this.CancelBooking}>Cancel</button>
+            </div>}
+
+          {this.state.show_price && <div class="book">
+            Your total price is {this.state.totalPrice}<br></br><br></br>
+            Do you want to proceed?<br></br><br></br>
+            <button type="submit" onClick={this.ConfirmBooking}>Confirm</button>
+          </div>}
+
         <table>
           <tr>
             <td>Id</td>
@@ -107,7 +152,11 @@ class App extends Component {
             <td>Durability</td>
             <td>Milegae</td>
           </tr>
-          {data.filter((item)=>(item.name.includes(this.state.searchValue))).map((data,index)=>(
+          {data.filter((item)=>{
+            if(item.name.toLowerCase().includes(this.state.searchValue.toLowerCase())||item.name.toUpperCase().includes(this.state.searchValue.toUpperCase())||item.code.toLowerCase().includes(this.state.searchValue.toLowerCase())||item.code.toUpperCase().includes(this.state.searchValue.toUpperCase())||item.durability==this.state.searchValue||item.max_durability==this.state.searchValue||item.mileage==this.state.searchValue)
+              return true
+            else 
+              return false}).map((data,index)=>(
             <tr key={index}>
               <td>{index+1}</td>
               <td>{data.name}</td>
@@ -121,39 +170,10 @@ class App extends Component {
           )
           }
         </table>
-          <div>
+          <div class="button-div">
             <button type="submit" onClick={this.book}>Book</button>
             <button type="submit" onClick={this.return}>Return</button>
           </div>
-
-          {this.state.book &&<div class="book">Book a Product
-              <select id="dropdown" onChange={this.onDropdown}>
-              {data.map((data,index)=>(
-                <option value={data.name}>{data.name}</option>
-              ))}
-              </select>
-              From:<DatePicker datefromat="MM/dd/yyyy" selected={this.state.startDate} onChange={this.onFromDate} /><br></br>
-              To:<DatePicker datefromat="MM/dd/yyyy" selected={this.state.startDate} onChange={this.onToDate} /><br></br>
-              <button type="submit" onClick={this.ProceedBooking}>Proceed</button>
-              <button type="submit" onClick={this.CancelBooking}>Cancel</button>
-            </div>}
-          
-            {this.state.return &&<div class="book">Return a Product
-              <select id="dropdown" onChange={this.onDropdown}>
-              {data.map((data,index)=>(
-                <option value={data.name}>{data.name}</option>
-              ))}
-              </select>
-              Mileage:<input type="text" onChange={this.onMileageChange}></input>
-              <button type="submit" onClick={this.ProceedReturn}>Proceed</button>
-              <button type="submit" onClick={this.CancelBooking}>Cancel</button>
-            </div>}
-
-          {this.state.show_price && <div class="book">
-            Your total price is {this.state.totalPrice}<br></br><br></br>
-            Do you want to proceed?<br></br><br></br>
-            <button type="submit" onClick={this.ConfirmBooking}>Confirm</button>
-          </div>}
       </div>
     );
   }
